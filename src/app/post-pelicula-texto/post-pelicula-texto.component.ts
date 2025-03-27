f91180744f54e4edee7000aa1882bca65db23f09
FMII <ivettemolina.009@gmail.com>
2025-03-26 23:46:28 -0600

    correcion en pelicula_texto

--- src/app/post-pelicula-texto/post-pelicula-texto.component.ts
+++ src/app/post-pelicula-texto/post-pelicula-texto.component.ts
@@ -2,18 +2,18 @@ import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
 import { PostPeliculaTexto } from '../interfaces/post-pelicula-texto';
 import { PostPeliculaTextoService } from '../services/post_pelicula_texto/post-pelicula-texto.service';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-post-pelicula-texto',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, NgIf],
   templateUrl: './post-pelicula-texto.component.html',
   styleUrls: ['./post-pelicula-texto.component.css']
 })
 export class PostPeliculaTextoComponent {
   peliculaTextoForm = new FormGroup({
     film_id: new FormControl<number | null>(null, [Validators.required]),
-    category_id: new FormControl<number | null>(null, [Validators.required]),
     title: new FormControl<string | null>(null, [
       Validators.required,
       Validators.minLength(3)
@@ -35,11 +35,17 @@ export class PostPeliculaTextoComponent {
       };
 
       this.postPeliculaTextoService.postPeliculaTexto(peliculaTexto).subscribe({
-        next: (response) => console.log('Película texto creada:', response),
+        next: (response) => {
+          console.log('Película texto creada:', response);
+          // Cierra el modal y reinicia el formulario
+          this.peliculaTextoForm.reset();
+          // Aquí puedes agregar un mensaje de éxito si lo deseas
+        },
         error: (error) => {
           console.error('Error al crear la película texto:', error);
           if (error.status === 400) {
             console.error('Detalles del error:', error.error);
+            // Aquí puedes mostrar un mensaje de error a nivel de la interfaz de usuario
           }
         },
       });
